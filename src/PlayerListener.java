8a981186afbe0c0204a386f0b305e2b3a1dc58d3
Kier Davis <kierdavis@gmail.con>
2013-05-19 19:43:11 +0100

    Initial code commit

---
+++ src/PlayerListener.java
@@ -0,0 +1,28 @@
+package com.kierdavis.kmail;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class PlayerListener implements Listener {
+    private UltraCommand plugin;
+    
+    public PlayerListener(UltraCommand plugin_) {
+        plugin = plugin_;
+    }
+    
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        if (event.isCancelled()) return;
+        
+        String cmdStr = event.getMessage();
+        String[] parts = cmdStr.split(' ');
+        String cmdName = parts[0].toLowerCase().substring(1);
+        
+        if (plugin.commands.containsKey(cmdName)) {
+            CustomCommand cmd = plugin.commands.get(cmdName);
+            cmd.execute(event.getPlayer());
+            event.setCancelled(true);
+        }
+    }
+}
--- src/UltraCommand.java
+++ src/UltraCommand.java
@@ -1,7 +1,26 @@
 package com.kierdavis.ultracommand;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UltraCommand extends JavaPlugin {
+    public Map<String, CustomCommand> commands;
     
+    public void onEnable() {
+        commands = new HashMap<String, CustomCommand>();
+        
+        CustomCommand c = new CustomCommand();
+        c.addText("&eSome text.");
+        c.addChat("&dSome chat.");
+        c.addCommand("/tell $p Some player comand.");
+        c.addConsoleCommand("/say Some server command.");
+        commands.put("test", c);
+        
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+    }
+    
+    public void onDisable() {
+        
+    }
 }
